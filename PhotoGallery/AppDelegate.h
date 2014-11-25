60649d0cbbddebc78b9d04c0fefc6a6cf669486f
Gennady Berezovsky <bergencroc@gmail.com>
2014-11-25 23:06:21 +0300

    Initial Commit

---
+++ PhotoGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoGallery
+//
+//  Created by Gennady Berezovsky on 25.11.14.
+//  Copyright (c) 2014 aiwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoGallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoGallery
+//
+//  Created by Gennady Berezovsky on 25.11.14.
+//  Copyright (c) 2014 aiwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
